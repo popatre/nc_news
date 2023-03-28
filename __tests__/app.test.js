23afc3b9e9eee78a6baad6afb1c72617435ef298
popatre <76402245+popatre@users.noreply.github.com>
2023-03-28 15:46:55 +0100

    co

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
 const seed = require("../db/seeds/seed.js");
 
+beforeEach(() => console.log("SOMETHING ELSE"));
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
